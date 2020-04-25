422f6f2a7897632b0cd34ff25ed0c29bd997980a
t0hmi <59023305+t0hmi@users.noreply.github.com>
2020-04-25 15:09:15 +0200

    2 commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const sr = ScrollReveal();
+
+sr.reveal(".bienvenue", { duration: 2000 });
+sr.reveal("#portfolio", {
+  origin: "top",
+  // easing: "ease",
+  duration: 2000,
+  delay: 500,
+});
+sr.reveal("h1", { duration: 1000 });
+sr.reveal(".txt", {
+  duration: 1000,
+});
+
+sr.reveal("#yo", {
+  duration: 1000,
+});
+
+sr.reveal(".region", {
+  duration: 1000,
+});
+
+sr.reveal(".space", {
+  duration: 1000,
+  delay: 750,
+});
+sr.reveal(".th", {
+  duration: 1000,
+  delay: 1000,
+});
+
+sr.reveal(".crea", {
+  duration: 1000,
+});
+sr.reveal(".develo", {
+  duration: 1000,
+  delay: 750,
+});
+
+sr.reveal(".logi", {
+  duration: 1000,
+  delay: 1000,
+});
+
+sr.reveal(".soft", {
+  duration: 1000,
+  delay: 1250,
+});
+
+sr.reveal("footer", {
+  duration: 2000,
+});
+
+sr.reveal(".fa-free-code-camp", {
+  duration: 2000,
+  delay: 1000,
+});
+
+sr.reveal(".fa-linkedin", {
+  duration: 2000,
+  delay: 1250,
+});
+
+sr.reveal(".fa-github", {
+  duration: 2000,
+  delay: 1500,
+});
+
+sr.reveal(".fa-envelope", {
+  duration: 2000,
+  delay: 1750,
+});
